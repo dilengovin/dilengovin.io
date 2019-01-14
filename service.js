0d6f65d9e3e38cd752012c18dda584d83dc7adf7
Dilen Govin <dilengovin@email.arizona.edu>
2019-01-14 13:11:25 -0700

    fixing local repository and fixing changes

--- service.js
+++ service.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send(JSON.stringify({ Hello: 'World'}));
+    res.send("Hello World!");
 });
 
 app.listen(port, function () {
